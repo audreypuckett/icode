1aad9659fc8d094c510e6f72526bf43194180e52
CHAO MENG <cmeng036@gmail.com>
2024-03-20 18:03:21 -0400

    Update ImageFilter.java
    Finished grayscale

--- icode/src/main/java/com/fdmgroup/icode/image/grayscale/ImageFilter.java
+++ icode/src/main/java/com/fdmgroup/icode/image/grayscale/ImageFilter.java
@@ -20,7 +20,22 @@ public class ImageFilter {
 	public void grayscaleFilter(ImageInfo imageInfo) {
 		BufferedImage bufferedImage = utils.getBufferedImage(imageInfo);
 		// ******************** CODE BELOW ****************
-
+		 int width = bufferedImage.getWidth();
+	     	 int height = bufferedImage.getHeight();
+	     	//iterate over each pixel of the image
+	     	 for(int y=0;y<height;y++) {
+		     for(int x=0;x<width;x++) {
+		         //get the rgb value
+		         int rgb = bufferedImage.getRGB(x,y);
+		         int R=(rgb>>16)&0xFF;
+		         int G=(rgb>>8)&0xFF;
+		         int B=rgb&0xFF;
+		         //get RGB average values
+		         int Avg=(R+G+B)/3;
+		         int newRGB=(Avg<<16)|(Avg<<8)|Avg;
+		         bufferedImage.setRGB(x, y, newRGB);
+		     }
+	         }
 		// ******************** CODE ABOVE ******************
 		utils.saveBufferedImage(bufferedImage, imageInfo.getSimpleName(), "grayscale");
 	}
